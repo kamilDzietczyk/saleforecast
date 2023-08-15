9f990a0461a4da5e156ae217479aef5262031daf
Kamil <kamil.dzietczyk1994@gmail.com>
2023-08-15 16:19:40 +0200

    1. Add checkstyle 2. Add jacoco

---
+++ src/main/java/com/example/saleforecast/SaleforecastApplication.java
@@ -0,0 +1,13 @@
+package com.example.saleforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleforecastApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SaleforecastApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/saleforecast/SaleforecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saleforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleforecastApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
